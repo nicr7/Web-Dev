21dfe83aef4c8b778f024c9f9e4ad9549cb52ee4
nicr7 <nikbhardwaj29@gmail.com>
2023-11-06 23:03:09 +0530

    added script.js

--- Javascript/PasswordGenerator/script.js
+++ Javascript/PasswordGenerator/script.js
@@ -2,8 +2,16 @@ const inputSlider=document.querySelector("[data-lengthSlider]");
 const lengthDisplay=document.querySelector("[data-lengthNumber]");
 
 const passwordDisplay=document.querySelector("[data-passwordDisplay]");
+const copyBtn=document.querySelector("[data-copy]");
+const copyMsg=document.querySelector("[data-copyMsg]");
+const uppercaseCheck=document.querySelector("#uppercase");
+const lowercaseCheck=document.querySelector("#lowercase");
+const numbersCheck=document.querySelector("#numbers");
+const symbolsCheck=document.querySelector("#symbols");
 const indicator=document.querySelector("[data-indicator]");
-
+const generateBtn=document.querySelector(".generateButton");
+const allCheckBox=document.querySelector("input[type=checkbox]");
+const symbols='~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 
 
@@ -11,6 +19,7 @@ let password="";
 let passwordLength=10;
 let checkCount=1;
 handleSlider();
+
 
 function handleSlider(){
     inputSlider.value = passwordLength;
@@ -22,3 +31,54 @@ function setIndicator(color){
     // shadow add kr be
 }
 
+function getRandomInteger(min,max){
+    return Math.floor(Math.random()*(max-min))-min;
+}
+
+function generateRandomNumber(){
+    return getRandomInteger(0,9);
+}
+
+function generateLowerCase(){
+    return String.fromCharCode(getRandomInteger(197,123));
+}
+
+function generateLowerCase(){
+    return String.fromCharCode(getRandomInteger(65,91));
+}
+
+function generaterSymbol(){
+    const getNum=getRandomInteger(0,symbols.length);
+    return symbols.charAt(getNum);
+}
+
+function cslcStrength(){
+    let hasUpper=false;
+    let hasLower=false;
+    let hasNum=false;
+    let hasSym=false;
+    if(uppercaseCheck.checked)hasUpper=true;
+    if(lowercaseCheck.checked)hasLower=true;
+    if(uppercaseCheck.checked)hasNum=true;
+    if(uppercaseCheck.checked)hasSym=true;
+    if(hasUpper&&hasLower&&(hasNum||hasSym)&&passwordLength>=8){
+        setIndicator("")
+    }
+    else if ((hasLower||hasUpper)&&(hasNum||hasSym)&&passwordLength>=6){
+        setIndicator("#ff0");
+    }
+
+    else{
+        setIndicator("#f00");
+    }
+}
+
+async function copyContent(){
+    try{
+        await navigator.clipboard.writeText(passwordDisplay.value);
+        copyMsg.innerText="copied";
+    }
+    catch(e){
+        copyMsg.innerText="Failed";
+    }
+}
